6dc4b3f1fa1c3ec07d49f1c04b112dd7ad47ffa9
Omar Guzmán <omaralexguzmanm21@gmail.com>
2023-04-11 00:08:41 -0600

    Consulta completa de datos (1er commit)

---
+++ app/Http/Livewire/ShowPosts.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Post;
+
+class ShowPosts extends Component
+{
+
+
+    public function render()
+    {
+        $posts = Post::all();
+
+        return view('livewire.show-posts', compact('posts'));
+    }
+}
